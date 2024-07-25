73e199289859e4ee4e13d01a8a350103a2e550b5
Vladimir Anisimov <aevien@hotmail.com>
2024-07-25 14:36:16 +1000

    Fixes

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 import './style.css'
-//import eruda from 'eruda'
 
 import InstantGamesBridge from './src/InstantGamesBridge.js'
 
 window.bridge = new InstantGamesBridge()
 window.instantGamesBridge = window.bridge
-//eruda.init()
